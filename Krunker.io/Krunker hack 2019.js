e038282a0bac286dbb6644bdcf3e34f73aebd56e
THEGUY <38544468+THEGUY3ds@users.noreply.github.com>
2019-06-15 14:44:50 +1000

    FIX

--- Krunker.io/Krunker hack 2019.js
+++ Krunker.io/Krunker hack 2019.js
@@ -1,42 +1,42 @@
- ==UserScript==
- @name         Best Krunker script 2019 Undetected Wallhack,Aimbot,No recoil
- @version      1.3
- @author       gpy-dev
- @include      ^(https)(www.)(.+)krunker.io((gameserverparty)=.+)$
- @grant        GM_xmlhttpRequest
- @run-at       document-start
- @require      httpscode.jquery.comjquery-3.3.1.min.js
- @namespace gpy
- ==UserScript==
+// ==UserScript==
+// @name         Best Krunker script 2019 Undetected Wallhack,Aimbot,No recoil
+// @version      1.3
+// @author       gpy-dev
+// @include      /^(https?:\/\/)?(www\.)?(.+)krunker\.io(|\/|\/\?(game|server|party)=.+)$/
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @namespace gpy
+// ==/UserScript==
 
 window.stop();
-document.innerHTML = ;
+document.innerHTML = "";
 
-                 
-                 
+// * * * * * * * * * * * * * * * * 
+// * * * * * * * * * * * * * * * * 
 
 const version = '1.3';
 
-                 
-                 
+// * * * * * * * * * * * * * * * * 
+// * * * * * * * * * * * * * * * * 
 
 GM_xmlhttpRequest({
-    method GET,
-    url document.location.origin,
-    onload res = {
+    method: "GET",
+    url: document.location.origin,
+    onload: res => {
         let html = res.responseText;
-        html = html.replace(game.[^.]+.js, '____.js');
-        html = html.replace(script (type=textjavascripts)data-cfasync(.s)script, `meta name=gpy_version content=${version}`);
+        html = html.replace(/game\.[^\.]+\.js/, '____.js');
+        html = html.replace(/<script (type="text\/javascript"\s)?data-cfasync(.|\s)*?<\/script>/, `<meta name="gpy_version" content="${version}">`);
         GM_xmlhttpRequest({
-            method GET,
-            url document.location.origin + 'libszip.js',
-            onload res = {
+            method: "GET",
+            url: document.location.origin + '/libs/zip.js',
+            onload: res => {
                 let zip = res.responseText;
-                zip = zip.replace(setInterval.);, '');
-                html = html.replace(script src=libszip.js.+script, `script${zip}script`);
-                html += 'script src=httpsraw.githack.comgpy-devkrunkermasterbypass.jsscript';
-                html += 'script src=httpsraw.githack.comgpy-devkrunkermasterhaxy.jsscript';
-                html += 'script src=httpsraw.githack.comgpy-devkrunkermastergame.jsscript';
+                zip = zip.replace(/setInterval.*?\);/, '');
+                html = html.replace(/<script src="libs\/zip\.js.+"><\/script>/, `<script>${zip}</script>`);
+                html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/bypass.js"></script>';
+                html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/haxy.js"></script>';
+                html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/game.js"></script>';
 
                 document.open();
                 document.write(html);
